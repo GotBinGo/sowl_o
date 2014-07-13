3b1b27638a27f37e8abcf1c2587ebb1e67adcc3a
Tomi <tomi@tomi.hu>
2014-07-13 16:40:03 +0200

    transition to conn.php

--- index.php
+++ index.php
@@ -55,25 +55,17 @@
       </div>
       <div id="Track_List" >
          <?php
-            include 'pass.php';
-            $con=mysqli_connect("127.0.0.1","bcophm_music",$pass,"bcophm_music");
-            if (mysqli_connect_errno()) 
-            {
-            	echo "Failed to connect to MySQL: " . mysqli_connect_error();
-            }
-            else
-            {
-            	$result = mysqli_query($con,"SELECT * FROM tracks WHERE user_id=0 AND (file_type='audio/mpeg' OR file_type='audio/mp3')");
-            	mysqli_close($con);
-            	$count = 0;
-            	while($row = mysqli_fetch_array($result)) {
-            		echo "<div class='List_Item' id='" . $count . "' sid='" . $row['file_name'] . "' onclick='Play_This(id)'>";
-            		echo $row['author_name'] . " - " . $row['track_name'] . "</div>";
-            		$count++;	
-            
-            	}
-            }
-            ?>	
+            include("conn.php");         
+            $result = mysqli_query($conn,"SELECT * FROM tracks WHERE user_id=0 AND (file_type='audio/mpeg' OR file_type='audio/mp3')");
+            mysqli_close($conn);
+            $count = 0;
+            while($row = mysqli_fetch_array($result))
+			{
+            	echo "<div class='List_Item' id='" . $count . "' sid='" . $row['file_name'] . "' onclick='Play_This(id)'>";
+            	echo $row['author_name'] . " - " . $row['track_name'] . "</div>";
+            	$count++;	
+            }            
+		?>	
       </div>
       <!--<button onclick="Prev_Item()"><<|</button>
          <button onclick="Play_Pause()">Play/Pause</button>
